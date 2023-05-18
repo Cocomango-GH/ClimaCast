4323907ea0030f3f5179adeecb08ea7cb1e77d02
cocomango-GH <111953271+Cocomango-GH@users.noreply.github.com>
2023-05-18 16:10:14 -0700

    databases

--- main_app/views.py
+++ main_app/views.py
@@ -52,25 +52,6 @@ def about(request):
     return render(request, 'about.html')
 
 
-def signup(request):
-    error_message = ''
-    if request.method == 'POST':
-        # This is how to create a 'user' form object
-        # that includes the data from the browser
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            # This will add the user to the database
-            user = form.save()
-            # This is how we log a user in via code
-            login(request, user)
-            return redirect('home')
-        else:
-            error_message = 'Invalid sign up - try again'
-    # A bad POST or a GET request, so render signup.html with an empty form
-    form = UserCreationForm()
-    context = {'form': form, 'error_message': error_message}
-    return render(request, 'registration/signup.html', context)
-
 
 @login_required
 def forecast(request):
@@ -143,3 +124,21 @@ def location_update(request):
             Location.objects.create(user=request.user, location=location, last_updated=datetime.now())
         return redirect('home')
 
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in via code
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
