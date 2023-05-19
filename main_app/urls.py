0aac3e3a4b0987bb7668d5a3c36806958a7d4297
cocomango-GH <111953271+Cocomango-GH@users.noreply.github.com>
2023-05-19 14:55:19 -0700

    init

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('climacast/', views.home, name='home'),
+    path('climacast/', views.home, name='weather'),
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
     path('location/create/', views.location_create, name='location_create'),
