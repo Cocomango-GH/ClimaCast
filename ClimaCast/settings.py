55b298d443ff16670242fe654b32d55bc5461689
cocomango-GH <111953271+Cocomango-GH@users.noreply.github.com>
2023-05-19 12:50:02 -0700

    init

--- ClimaCast/settings.py
+++ ClimaCast/settings.py
@@ -60,10 +60,10 @@ ROOT_URLCONF = 'ClimaCast.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            #chat saidputthis to acesse forecast.html
-             os.path.join(BASE_DIR, 'main_app', 'Templates'),
-        ],
+        'DIRS': [],
+            # #chat saidputthis to acesse forecast.html
+            #  os.path.join(BASE_DIR, 'main_app', 'Templates'),
+        
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
